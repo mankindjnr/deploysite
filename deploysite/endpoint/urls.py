546c59ce446e75a73db9192fb1c3dd16afba01ec
mankindjnr <kamos@stu.kemu.ac.ke>
2023-09-29 07:13:37 +0300

    deploy test 1

---
+++ deploysite/endpoint/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('another', views.another, name='another'),
+    path('stages', views.stages, name="stages")
+]
