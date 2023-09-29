9dceb0883da480eb2e6f3dcb1e582456408beb75
mankindjnr <kamos@stu.kemu.ac.ke>
2023-09-29 08:34:21 +0300

    test 4

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -o errexit
 set -o pipefail
 
+POETRY_VERSION="1.6.1"
+curl -sSL https://install.python-poetry.org | POETRY_VERSION=$POETRY_VERSION python -
 # Activate the Poetry virtual environment
 source "$(poetry env info --path)/bin/activate"
 
@@ -13,5 +15,5 @@ poetry lock
 poetry install
 
 # Collect static files and migrate the database
-python manage.py collectstatic --no-input
+#python manage.py collectstatic --no-input
 python manage.py migrate
