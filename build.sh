f7a12944f2856db95efb00543bf721b173b2a977
mankindjnr <kamos@stu.kemu.ac.ke>
2023-09-29 08:48:13 +0300

    build check

--- build.sh
+++ build.sh
@@ -3,17 +3,20 @@
 set -o errexit
 set -o pipefail
 
-POETRY_VERSION="1.6.1"
-curl -sSL https://install.python-poetry.org | POETRY_VERSION=$POETRY_VERSION python -
+echo "past the set"
 # Activate the Poetry virtual environment
 source "$(poetry env info --path)/bin/activate"
+
+echo "past the source"
 
 # Ensure the lock file is up-to-date
 poetry lock
 
+echo "past the lock"
 # Install project dependencies
 poetry install
 
+echo "past the install"
 # Collect static files and migrate the database
 #python manage.py collectstatic --no-input
 python manage.py migrate
